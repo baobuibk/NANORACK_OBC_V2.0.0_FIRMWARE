fac41785309d3c66208ffada564dc9bf2abf66df
Hieu Cao <itwasme.ulrich@gmail.com>
2025-07-25 08:41:19 +0700

    [-th] Add SPI transfer, edit cli, fix script

--- CM4/Debug/0_DEV/M0_App4/OS4/scheduler.d
+++
@@ -1,3 +0,0 @@
-0_DEV/M0_App4/OS4/scheduler.o: ../0_DEV/M0_App4/OS4/scheduler.c \
- ../0_DEV/M0_App4/OS4/scheduler.h
-../0_DEV/M0_App4/OS4/scheduler.h:
--- CM4/Debug/0_DEV/M1_Driver4/Shared_REG/shared_reg.d
+++
@@ -1,4 +0,0 @@
-0_DEV/M1_Driver4/Shared_REG/shared_reg.o: \
- ../0_DEV/M1_Driver4/Shared_REG/shared_reg.c \
- ../0_DEV/M1_Driver4/Shared_REG/shared_reg.h
-../0_DEV/M1_Driver4/Shared_REG/shared_reg.h:
